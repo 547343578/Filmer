336d875a4697aec7008c937e52916fdc95cc5b20
547343578 <wanglucas5@gmail.com>
2021-07-24 11:17:09 +0200

    update

--- src/main/java/com/filmer/FilmerApplication.java
+++ src/main/java/com/filmer/FilmerApplication.java
@@ -2,10 +2,16 @@ package com.filmer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class FilmerApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(FilmerApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmerApplication.class, args);
